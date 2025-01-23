934b2dc560e3f99840efbeab012c647966ab2ebb
PARTH PARMAR <parthparmar@PARTHs-MacBook-Air.local>
2025-01-23 11:16:34 +0530

    add webToLead Project

--- Data/details.js
+++ Data/details.js
@@ -8,7 +8,7 @@ module.exports={
             "Technology": "Salesforce, Salesforce LWC, Apex, JavaScript, jQuery, CSS, GitHub",
             "RoleDetail": "I had developed two apps: one is a rule engine used to define rules for specific clause sets, and the other is a clause selection tool utilized to automate order form creation. \r\n\r\nI structured the rule engine to enable users to define rules hierarchically, allowing for both parent-to-child and child-to-parent relationships. This design empowers users to create more specific rules tailored to their needs. \r\n\r\nThe clause selection engine features a picklist containing all eligible clauses, which are evaluated upon record load according to our predefined rules. \r\n\r\nAlso implemented some other features like approvals, email alerts, flows etc.",
             "Imagelinks": "https://i.postimg.cc/wM0Swr6m/img1.png,https://i.postimg.cc/rs8BCTLT/img2.png,https://i.postimg.cc/wx28x1hj/img3.png,https://i.postimg.cc/sX9FhXqK/img4.png",
-            "Id": "a02dM000003ZfMxQAK",
+            "Id": "1",
             "Description": "<p> Specific to standard and non-standard commercial terms, there are scenarios where deal strategy acts as a blocker or causes unnecessary friction for sales to execute on an Order Form. </p> <p>The objective is to build a solution in CLM to automate the commercial terms to:</p> <ul> <li>Increase Sales autonomy by increasing deal velocity and reducing deal friction and touch points.</li> <li>Reduce operational cost around the deal cycle by reducing Order Form manual touch points.</li> <li>Standardize the pre-approved Legal and DSO commercial terms.</li> </ul>"
         },
         {
@@ -16,7 +16,7 @@ module.exports={
             "Technology": "Salesforce, Salesforce LWC, Apex, JavaScript, jQuery, CSS, GitHub, .Net",
             "RoleDetail": "I was part of big team where my responsibilities included proposing optimized solutions for new functionalities, swiftly resolving any blocking issues, analyzing debug logs to pinpoint issues, offering workarounds for limitations in Conga products, and conducting proof-of-concept trials for proposed solutions.",
             "Imagelinks": null,
-            "Id": "a02dM000003ZinhQAC",
+            "Id": "2",
             "Description": "<p> The project involved leveraging Conga Cloud to enhance the capabilities of the existing system. The primary goal was to overcome the limitation of pricing and configuring only 250 lines. </p> <p> The project aimed to introduce new features to augment the functionality of the current solution. Overall, the objective was to extend and improve the system's capabilities to better meet the customer's needs and accommodate future growth. </p>"
         },
         {
@@ -24,16 +24,24 @@ module.exports={
             "Technology": "HTML, CSS3, jQuery, Bootstrap, PHP, MySQL",
             "RoleDetail": "Developed and implemented frontend pages such as The Brand and Signature Collection, ensuring a seamless and visually appealing user experience with HTML, CSS, Bootstrap, and JavaScript/jQuery.  \r\n\r\nContributed to the development of backend functionalities, including user authentication, product management, and order processing, ensuring smooth website operations and secure transactions for both customers and sellers.",
             "Imagelinks": null,
-            "Id": "a02dM000003ZjF3QAK",
+            "Id": "3",
             "Description": "<p> Gharenu Jewelry Marketplace is an e-commerce platform dedicated to providing a seamless and elegant shopping experience for customers interested in selling and purchasing jewelry. </p> <p> With a focus on authenticity, quality, and variety, Gharenu.com offers a curated selection of jewelry pieces ranging from traditional to contemporary designs. </p>"
         },
         {
             "Title": "Leave App",
             "Technology": "Salesforce, Apex, Lightning",
-            "RoleDetail": "we have designed a basic lighting app.",
+            "RoleDetail": "i have designed a basic lighting app.",
             "Imagelinks": "https://i.postimg.cc/d3qw4Xy2/Leave-App-SS1.png,https://i.postimg.cc/63VBVwy6/Leave-App-SS2.png",
-            "Id": "a02dM000004GwaPQAS",
+            "Id": "4",
             "Description": "<p>The customer wanted a leave management app on Salesforce with the basic features.</p> <strong>Requirement:</strong> <ul> <li>It must be implemented in LWC.</li> <li>Want to leverage user's fields in the app.</li> <li>When the user submits the leave, approval should go to his manager.</li> <li>UI should be simple and easy to use.</li> <li>The user can revoke the leave request.</li> <li>All leave request history should be visible on the UI.</li> <li>The manager must receive a mail when a user submits a leave request.</li> <li>The app must be scalable to medium size teams.</li> </ul>"
+        },
+        {
+            "Title": "Web-to-Lead Form",
+            "Technology": "Salesforce, Apex, Lightning, WordPress",
+            "RoleDetail": "Salesforce offers an out-of-the-box Web-to-Lead form; however, it lacks a file upload feature. To address this limitation, I customized the form and integrated it into a WordPress site.",
+            "Imagelinks": "https://i.postimg.cc/kGMx3dR8/Screenshot-2025-01-23-at-11-11-21-AM.png",
+            "Id": "5",
+            "Description": "<p>The user required a <strong>Web-to-Lead form</strong> with <em>file upload functionality</em>, where the uploaded file would be saved as a <strong>note and attachment</strong> on the associated lead record.</p>"
         }
     ],
     "Id": "a01dM00000KReKSQA1",
